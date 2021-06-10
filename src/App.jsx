d1d3c0e044d89f5a21cdbd21b10edfa21a6bde02
Marcelo Bettini <marcelobettini@yahoo.com.ar>
2021-06-10 13:45:08 -0300

    Preparando el uso del hook UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom"; //BrowserRouter as Router for short
 import Base from "./components/Base";
 import Inicio from "./components/Inicio";
 function App() {
   return (
     <BrowserRouter>
+    <Link className="btn btn-success btn-sm m-2" to="/"> Base </Link>
+    <Link className="btn btn-success btn-sm m-2" to="/contact"> Contact </Link>
+    <Link className="btn btn-success btn-sm m-2" to="/inicio"> Inicio </Link>
     <Switch>
       <Route exact path="/">
         <Base></Base>
       </Route>
     </Switch>
     <Switch>
-      <Route path="/contact">Contact Form goes here</Route>
+      <Route path="/contact">Contact Form goes here (no component)</Route>
     </Switch>
     <Switch>
       <Route path="/inicio">
--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -4,6 +4,7 @@ const Base = () => {
   return (
     <div>
       <h3 className="display-3">Página de Base de la URL "/"</h3>
+      <p>Usamos el hook UseEffect</p>
     </div>
   )
 }
