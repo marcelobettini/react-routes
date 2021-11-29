d02e3f1121737bba6f8e826e5d2503b4f8f69052
unknown <marcelobettini@yahoo.com.ar>
2021-11-28 21:06:16 -0300

    fetch más conciso

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,20 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 const Data = () => {
-  const [data, setData] = useState([{}]);
-  const [error, setError] = useState("");
-
+  const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        if (!response.ok) {
-          return setError("Falló la conexión con la API");
-        }
-        return response.json();
-      })
-      .then((rawdata) => setData(rawdata))
-      .catch((err) => setError(err));
-  }, []); //Si no se pasa el array vacío, entra en loop
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((rawdata) => setData(rawdata))
+    );
+  }, []);
 
   return (
     <Fragment>
